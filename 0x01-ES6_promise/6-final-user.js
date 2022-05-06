eb1dcfa21bcd1d979c072ed7609dc26b424ea110
alixvox <alixvox@gmail.com>
2022-05-05 23:53:01 -0500

    Fixing

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return [
     await signUpUser(firstName, lastName)
       /* no need for a catch signUpUser has only one resolve */
-      .then((response) => ({ status 'fulfilled', value: response })),
+      .then((response) => ({ status: 'fulfilled', value: response })),
     await uploadPhoto(fileName)
       /* uploadPhoto only has a reject no need for a then */
-      .catch((response) => ({ status 'rejected', value: response.toString() })),
+      .catch((response) => ({ status: 'rejected', value: response.toString() })),
   ];
 }
