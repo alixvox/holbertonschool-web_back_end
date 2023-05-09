daa6b8198cfcfb54b5ec98bde23bf5e8097d84c6
alixvox <alixvox@gmail.com>
2023-05-08 19:35:17 -0500

    Changing 9 to fit

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,10 +2,10 @@
 '''A type-annotated function that takes a list of str and
 returns a list of tuples of the str and index in the list.'''
 
-from typing import List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    '''Returns a list of tuples from list lst where each
-    tuple contains a str from lst and its corresponding length'''
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''Returns a list of tuples from an Iterable of Seuqnces lst where
+    each tuple contains a Sequence from lst and its corresponding length'''
     return [(i, len(i)) for i in lst]
