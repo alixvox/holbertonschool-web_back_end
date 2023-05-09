579e43e6145f2b33d18f694283379f822f4a8251
alixvox <alixvox@gmail.com>
2023-05-08 19:50:31 -0500

    Changing 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ and an int and returns them as a tuple.'''
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Returns a str k and an int OR float v and
     returns them as a tuple'''
     return (k, v)
