22f8280a297d87160de64c385e3da76fda3e1959
Alex Leeper <alixvox@gmail.com>
2023-05-19 05:06:54 -0500

    Adding task 2!

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -12,6 +12,9 @@ import re
 from typing import List
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 class RedactingFormatter(logging.Formatter):
     """
     Formatter class for logging module with redaction capability.
@@ -67,3 +70,16 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{field}=.+?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a logging.Logger object.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
