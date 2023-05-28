5945a333edcb293fa831bf45d6730d2d3b469dd3
Alex Leeper <alixvox@gmail.com>
2023-05-28 17:08:26 -0500

    Task 0 FINALLY

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -37,6 +37,8 @@ def before_request_func():
     if auth.authorization_header(request) is None:
         abort(401)
 
+    request.current_user = auth.current_user(request)
+
     if auth.current_user(request) is None:
         abort(403)
 
