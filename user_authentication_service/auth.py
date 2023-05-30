81a7134bb7139f52e35f36a518f8e9729ef896b8
Alex Leeper <alixvox@gmail.com>
2023-05-29 19:46:25 -0500

    Fixins

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
             if user:
                 raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
 
         return user
