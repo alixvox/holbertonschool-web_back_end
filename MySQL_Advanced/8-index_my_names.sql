e91c140c7697f9c85fe75bd9486ed4dea5033185
Alex Leeper <alixvox@gmail.com>
2023-06-20 16:34:57 -0500

    changin files

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,4 @@
 -- 8-index_my_names.sql
 -- Indexes names starting with the first letter.
 
-ALTER TABLE names
-ADD COLUMN name_first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
-
 CREATE INDEX idx_name_first ON names (name(1));
