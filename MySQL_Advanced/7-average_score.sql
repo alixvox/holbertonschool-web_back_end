33fd28bb0ab7ec8a0f9f72405eea82251c6b281e
Alex Leeper <alixvox@gmail.com>
2023-06-20 15:09:44 -0500

    adding new

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,19 @@
 -- 7-average_score.sql
--- This creates an average score for each student.
+-- This updates the average score for each student.
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
    DECLARE avg_score FLOAT;
-
+   
    -- Compute the average score
    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-
+   
    -- Update the average score in the users table
    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END;//
+END;
+//
+
 DELIMITER ;
