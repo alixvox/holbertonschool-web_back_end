fc031e9738900cccc395077385818a433a31e80b
Alex Leeper <alixvox@gmail.com>
2023-06-20 16:40:45 -0500

    adding file

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- 9-index_name_score.sql
 -- Add column for first letter of name
 
-CREATE INDEX idx_name_first_score ON names (name_first_letter, score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
