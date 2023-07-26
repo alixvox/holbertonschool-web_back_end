b1f8d2462bd9ea26a7c35ce402fa5604394e5d0e
Alex Leeper <alixvox@gmail.com>
2023-07-26 15:36:44 -0500

    bulkiodonkus

---
+++ GraphQL_API/server/schema/schema.js
@@ -0,0 +1,51 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = require('graphql');
+const _ = require('lodash');
+
+const tasks = [
+    {
+        id: '1',
+        title: 'Create your first webpage',
+        weight: 1,
+        description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+    },
+    {
+        id: '2',
+        title: 'Structure your webpage',
+        weight: 1,
+        description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+
+    }
+];
+
+const TaskType = new GraphQLObjectType({
+    name: 'Task',
+    fields: {
+        id: { type: GraphQLString },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: { type: GraphQLString }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        task: {
+            type: TaskType,
+            args: { id: {type: GraphQLString } },
+            resolve(parent, args) {
+                // Use lodash to find the task with the given id
+                return _.find(tasks, { id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
